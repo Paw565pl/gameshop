8bf41f3ab82cfa8e6289360a8be948c18abb00d8
Paw565pl <paw565pl@gmail.com>
2023-12-06 17:44:32 +0100

    created base model class

--- core/models.py
+++ core/models.py
@@ -3,7 +3,14 @@ from djongo import models
 
 
 # Create your models here.
-class User(AbstractUser):
+class BaseModel(models.Model):
+    _id = models.ObjectIdField(primary_key=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel, AbstractUser):
     email = models.EmailField("email address", unique=True)
     first_name = None
     last_name = None
