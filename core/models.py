e8430c21ac840f423199304f75f6ecaab8935e58
Paw565pl <paw565pl@gmail.com>
2023-12-06 16:49:06 +0100

    created custom user model

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from djongo import models
+
+
+# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField("email address", unique=True)
+    first_name = None
+    last_name = None
