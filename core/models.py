59ec7f414a5b2db065cf1044f4ebf01e15757b8f
Paw565pl <paw565pl@gmail.com>
2023-12-09 13:19:48 +0100

    deleted base model class

--- core/models.py
+++ core/models.py
@@ -3,14 +3,9 @@ from djongo import models
 
 
 # Create your models here.
-class BaseModel(models.Model):
-    _id = models.ObjectIdField(primary_key=True)
-
-    class Meta:
-        abstract = True
 
 
-class User(BaseModel, AbstractUser):
+class User(AbstractUser):
     email = models.EmailField("email address", unique=True)
     first_name = None
     last_name = None
