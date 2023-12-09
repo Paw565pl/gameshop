d40aa637baeee9802cfe177415acdf0b64540395
Paw565pl <paw565pl@gmail.com>
2023-12-09 20:32:07 +0100

    changed url params naming to snake case

--- core/pagination.py
+++ core/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 30
-    page_size_query_param = "pageSize"
+    page_size_query_param = "page_size"
     max_page_size = 100
--- games/filters.py
+++ games/filters.py
@@ -3,14 +3,14 @@ from django_filters import rest_framework as filters
 
 class GameFilter(filters.FilterSet):
     name = filters.CharFilter(field_name="name", lookup_expr="icontains")
-    minReleaseYear = filters.NumberFilter(
+    min_release_year = filters.NumberFilter(
         field_name="released", lookup_expr="year__lte"
     )
-    maxReleaseYear = filters.NumberFilter(
+    max_release_year = filters.NumberFilter(
         field_name="released", lookup_expr="year__gte"
     )
-    maxMetacritic = filters.NumberFilter(field_name="metacritic", lookup_expr="gte")
-    minMetacritic = filters.NumberFilter(field_name="metacritic", lookup_expr="lte")
+    min_metacritic = filters.NumberFilter(field_name="metacritic", lookup_expr="lte")
+    max_metacritic = filters.NumberFilter(field_name="metacritic", lookup_expr="gte")
     genre = filters.CharFilter(
         label="Genre name", field_name="slug", method="filter_by_genre"
     )
