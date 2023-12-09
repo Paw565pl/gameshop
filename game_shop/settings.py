821eb7064960549efb31f8619fb41e90626f4269
Paw565pl <paw565pl@gmail.com>
2023-12-09 20:21:01 +0100

    created filters for game view set

--- game_shop/settings.py
+++ game_shop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "core",
     "games",
 ]
@@ -133,3 +134,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+    ]
+}
