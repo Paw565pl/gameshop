124c9c15fb0bae0b194b0278bfd7274718092a60
Paw565pl <paw565pl@gmail.com>
2023-12-09 15:24:59 +0100

    created games view set

---
+++ games/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+
+router.register(r"games", views.GameViewSet)
+
+urlpatterns = router.urls
--- games/views.py
+++ games/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+
+from .models import Game
+from core.pagination import StandardPagination
+from .serializers import GameSerializer
+
 
 # Create your views here.
+class GameViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin
+):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    pagination_class = StandardPagination
