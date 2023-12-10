cd587c4afef36052472a16e8c7ee8a21040d9a1b
Paw565pl <paw565pl@gmail.com>
2023-12-10 19:52:45 +0100

    cleaned up imports

--- games/urls.py
+++ games/urls.py
@@ -1,11 +1,11 @@
 from rest_framework import routers
-from rest_framework_nested import routers
+from rest_framework_nested import routers as nested_routers
 from . import views
 
 router = routers.DefaultRouter()
 
 router.register(r"games", views.GameViewSet)
-game_router = routers.NestedDefaultRouter(router, r"games", lookup="game")
+game_router = nested_routers.NestedDefaultRouter(router, r"games", lookup="game")
 game_router.register(r"reviews", views.ReviewViewSet, basename="game-reviews")
 
 urlpatterns = router.urls + game_router.urls
