654a5f41a8fb70e68554cd74faf9687ade34a2d9
Paw565pl <paw565pl@gmail.com>
2023-12-10 19:49:00 +0100

    created serializers for game app models

--- games/serializers.py
+++ games/serializers.py
@@ -27,9 +27,56 @@ class ScreenshotSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(read_only=True)
+
     class Meta:
         model = Review
         fields = "__all__"
+
+    def create(self, validated_data):
+        username = self.context["request"].user.username
+        game_id = self.context["view"].kwargs["game_pk"]
+
+        game_queryset = Game.objects.filter(pk=game_id)
+
+        has_review = len(list(game_queryset.filter(reviews={"author": username}))) != 0
+        if has_review:
+            raise serializers.ValidationError("You have already added a review.")
+
+        game = game_queryset.first()
+        new_review = {
+            "author": username,
+            "content": validated_data["content"],
+        }
+        game.reviews.append(new_review)
+        game.save()
+
+        return new_review
+
+    def update(self, instance, validated_data):
+        username = self.context["request"].user.username
+        game_id = self.context["view"].kwargs["game_pk"]
+
+        game = Game.objects.filter(pk=game_id, reviews={"author": username}).first()
+        reviews = game.reviews
+        updated_review = None
+
+        for review in reviews:
+            if review["author"] == username:
+                review["content"] = validated_data.get("content", review["content"])
+                updated_review = review
+                break
+
+        game.save()
+        return updated_review
+
+
+class GameReviewSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Game
+        fields = ["reviews"]
 
 
 class GameSerializer(serializers.ModelSerializer):
