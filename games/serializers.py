dfc228ed5cc79b9904a4b33bbf73b9707d01989d
Paw565pl <paw565pl@gmail.com>
2023-12-14 20:33:41 +0100

    adjusted serializers for the changed models

--- games/serializers.py
+++ games/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Game, Genre, Platform, Developer, Screenshot, Review
+from .models import Game, Genre, Platform, Developer, Screenshot, Review, DisLike, Like
 from core.utils import get_mongo_collection
 
 
@@ -35,57 +35,60 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = "__all__"
 
-    def create(self, validated_data):
-        username = self.context["request"].user.username
-        game_id = self.context["view"].kwargs["game_pk"]
-        content = validated_data["content"]
-
-        game_queryset = Game.objects.filter(pk=game_id)
-
-        has_review = len(list(game_queryset.filter(reviews={"author": username}))) != 0
-        if has_review:
-            raise serializers.ValidationError("You have already added a review.")
-
-        game = game_queryset.first()
-        new_review = {
-            "author": username,
-            "content": content,
-        }
-        game.reviews.append(new_review)
-        game.save()
-
-        return new_review
-
-    def update(self, instance, validated_data):
-        username = self.context["request"].user.username
-        game_id = int(self.context["view"].kwargs["game_pk"])
-        new_content = validated_data.get("content")
-
-        games_collection = get_mongo_collection("games_game")
-        games_collection.update_one(
-            {"id": game_id, "reviews.author": username},
-            {"$set": {"reviews.$.content": new_content}},
-        )
-        updated_review = games_collection.find_one(
-            {"id": game_id, "reviews.author": username}, {"reviews.$": 1}
-        )["reviews"][0]
-
-        return updated_review
-
-
-class GameReviewSerializer(serializers.ModelSerializer):
-    reviews = ReviewSerializer(many=True)
-
-    class Meta:
-        model = Game
-        fields = ["reviews"]
+    # def create(self, validated_data):
+    #     username = self.context["request"].user.username
+    #     game_id = self.context["view"].kwargs["game_pk"]
+    #     content = validated_data["content"]
+    #
+    #     game_queryset = Game.objects.filter(pk=game_id)
+    #
+    #     has_review = len(list(game_queryset.filter(reviews={"author": username}))) != 0
+    #     if has_review:
+    #         raise serializers.ValidationError("You have already added a review.")
+    #
+    #     game = game_queryset.first()
+    #     new_review = {
+    #         "author": username,
+    #         "content": content,
+    #     }
+    #     game.reviews.append(new_review)
+    #     game.save()
+    #
+    #     return new_review
+    #
+    # def update(self, instance, validated_data):
+    #     username = self.context["request"].user.username
+    #     game_id = int(self.context["view"].kwargs["game_pk"])
+    #     new_content = validated_data.get("content")
+    #
+    #     games_collection = get_mongo_collection("games_game")
+    #     games_collection.update_one(
+    #         {"id": game_id, "reviews.author": username},
+    #         {"$set": {"reviews.$.content": new_content}},
+    #     )
+    #     updated_review = games_collection.find_one(
+    #         {"id": game_id, "reviews.author": username}, {"reviews.$": 1}
+    #     )["reviews"][0]
+    #
+    #     return updated_review
 
 
 class GameSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    dislikes_count = serializers.SerializerMethodField()
+
     genres = GenreSerializer(many=True)
     platforms = PlatformSerializer(many=True)
     developers = DeveloperSerializer(many=True)
     screenshots = ScreenshotSerializer(many=True)
+
+    @staticmethod
+    def get_likes_count(game):
+        return game.likes.count()
+
+    @staticmethod
+    def get_dislikes_count(game):
+        return game.dislikes.count()
 
     class Meta:
         model = Game
@@ -94,12 +97,13 @@ class GameSerializer(serializers.ModelSerializer):
             "name",
             "name_original",
             "slug",
-            "likes",
             "released",
             "metacritic",
             "background_image",
             "website",
             "description_raw",
+            "likes_count",
+            "dislikes_count",
             "genres",
             "platforms",
             "developers",
