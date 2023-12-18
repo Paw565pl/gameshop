b36bdbf97d6b3d317b1f0255be135c7b65caf57a
Paw565pl <paw565pl@gmail.com>
2023-12-18 11:27:21 +0100

    moved screenshots of given game to a nested resource

--- games/urls.py
+++ games/urls.py
@@ -7,5 +7,8 @@ router.register(r"games", views.GameViewSet)
 
 game_router = routers.NestedDefaultRouter(router, r"games", lookup="game")
 game_router.register(r"reviews", views.ReviewViewSet, basename="game-reviews")
+game_router.register(
+    r"screenshots", views.ScreenshotViewSet, basename="game-screenshots"
+)
 
 urlpatterns = router.urls + game_router.urls
--- games/views.py
+++ games/views.py
@@ -1,19 +1,30 @@
-from rest_framework import viewsets, mixins, permissions, exceptions
+from rest_framework import viewsets, permissions, exceptions
 
 from .filters import GameFilter
-from .models import Game, Review
+from .models import Game, Review, Screenshot
 from .permissions import IsAuthorOrReadOnly
-from .serializers import GameSerializer, ReviewSerializer
+from .serializers import GameSerializer, ReviewSerializer, ScreenshotSerializer
 
 
 # Create your views here.
-class GameViewSet(
-    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin
-):
+class GameViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
     filterset_class = GameFilter
     ordering_fields = ["name", "released", "metacritic"]
+
+
+class ScreenshotViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ScreenshotSerializer
+
+    def get_queryset(self):
+        game_id = self.kwargs["game_pk"]
+        game = Game.objects.filter(pk=game_id).count()
+
+        if game == 0:
+            raise exceptions.NotFound()
+
+        return Screenshot.objects.filter(game__id=game_id)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
