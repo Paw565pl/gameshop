1b155b94a0ab74c66704a957ab1d7929ceb12e7a
Paw565pl <paw565pl@gmail.com>
2023-12-18 10:48:19 +0100

    removed like and dislike models, added djongo manager to models, introduced is_positive field to review model

--- games/models.py
+++ games/models.py
@@ -8,6 +8,8 @@ class Genre(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
     background_image = models.URLField(null=True, blank=True)
 
+    objects = models.DjongoManager()
+
     def __str__(self):
         return self.name
 
@@ -16,6 +18,8 @@ class Platform(models.Model):
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
 
+    objects = models.DjongoManager()
+
     def __str__(self):
         return self.name
 
@@ -23,6 +27,8 @@ class Platform(models.Model):
 class Developer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
+
+    objects = models.DjongoManager()
 
     def __str__(self):
         return self.name
@@ -33,27 +39,18 @@ class Screenshot(models.Model):
     width = models.IntegerField()
     height = models.IntegerField()
 
+    objects = models.DjongoManager()
+
 
 class Review(models.Model):
     author = models.CharField(max_length=255)
+    is_positive = models.BooleanField()
     content = models.TextField()
+
+    objects = models.DjongoManager()
 
     def __str__(self):
         return f"review by {self.author}"
-
-
-class Like(models.Model):
-    author = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return f"like by {self.author}"
-
-
-class DisLike(models.Model):
-    author = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return f"dislike by {self.author}"
 
 
 class Game(models.Model):
@@ -68,14 +65,16 @@ class Game(models.Model):
     website = models.URLField(null=True, blank=True)
     description_raw = models.TextField(null=True, blank=True)
 
-    likes = models.ArrayReferenceField(to=Like, on_delete=models.PROTECT)
-    dislikes = models.ArrayReferenceField(to=DisLike, on_delete=models.PROTECT)
-
     genres = models.ArrayReferenceField(to=Genre, on_delete=models.PROTECT)
     platforms = models.ArrayReferenceField(to=Platform, on_delete=models.PROTECT)
     developers = models.ArrayReferenceField(to=Developer, on_delete=models.PROTECT)
-    screenshots = models.ArrayReferenceField(to=Screenshot, on_delete=models.PROTECT)
-    reviews = models.ArrayReferenceField(to=Review, on_delete=models.PROTECT)
+
+    screenshots = models.ArrayReferenceField(to=Screenshot)
+
+    reviews = models.ArrayReferenceField(to=Review)
+    reviews_count = models.IntegerField(default=0)
+
+    objects = models.DjongoManager()
 
     class Meta:
         indexes = [
