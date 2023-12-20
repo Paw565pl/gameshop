41c9db24c38420354810f57e4430d18491cf096f
Paw565pl <paw565pl@gmail.com>
2023-12-20 12:30:36 +0100

    fixed pagination warnings

--- games/views.py
+++ games/views.py
@@ -9,7 +9,7 @@ from .utils import check_if_game_exists
 
 # Create your views here.
 class GameViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Game.objects.all()
+    queryset = Game.objects.order_by("id")
     serializer_class = GameSerializer
     filterset_class = GameFilter
     ordering_fields = ["name", "released", "metacritic"]
@@ -22,7 +22,7 @@ class ScreenshotViewSet(viewsets.ReadOnlyModelViewSet):
         game_id = self.kwargs["game_pk"]
         check_if_game_exists(game_id)
 
-        return Screenshot.objects.filter(game__id=game_id)
+        return Screenshot.objects.filter(game__id=game_id).order_by("id")
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
@@ -33,7 +33,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
         game_id = self.kwargs["game_pk"]
         check_if_game_exists(game_id)
 
-        return Review.objects.filter(game__id=game_id)
+        return Review.objects.filter(game__id=game_id).order_by("id")
 
     def create(self, request, *args, **kwargs):
         game_id = self.kwargs["game_pk"]
