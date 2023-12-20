cde855e97c6e074a182055f5f7d4ab0cdb03f11d
Paw565pl <paw565pl@gmail.com>
2023-12-20 10:38:13 +0100

    customized admin site header

--- game_shop/urls.py
+++ game_shop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Game Shop Admin"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
