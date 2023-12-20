94957cfa9c67f16195aa6704aadff176e1c6fa8e
Paw565pl <paw565pl@gmail.com>
2023-12-20 11:10:27 +0100

    created game admin panel

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,29 @@
+from django import forms
 from django.contrib import admin
 
+from core.mixins import ExportCsvMixin
+from .models import Game
+
+
 # Register your models here.
+class CommonAdmin(admin.ModelAdmin, ExportCsvMixin):
+    actions = ["export_as_csv"]
+    list_display = ["name"]
+
+
+class GameAdminForm(forms.ModelForm):
+    class Meta:
+        model = Game
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["screenshots"].required = False
+        self.fields["reviews"].required = False
+
+
+@admin.register(Game)
+class GameAdmin(CommonAdmin):
+    form = GameAdminForm
+    search_fields = ["name__icontains", "slug__icontains"]
+    list_filter = ["genres", "platforms", "developers"]
