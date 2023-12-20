359fe507f9796fbeb002fccac163cec054047157
Paw565pl <paw565pl@gmail.com>
2023-12-20 15:23:39 +0100

    customized exception message

--- games/utils.py
+++ games/utils.py
@@ -7,4 +7,4 @@ def check_if_game_exists(game_id: int):
     game = Game.objects.filter(pk=game_id).count()
 
     if game == 0:
-        raise exceptions.NotFound()
+        raise exceptions.NotFound("Game not found")
