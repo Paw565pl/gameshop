68bb6a109ea36434be8e2c3208aa239c397d2026
Paw565pl <paw565pl@gmail.com>
2023-12-27 15:53:04 +0100

    nested all api routes correctly

--- game_shop/urls.py
+++ game_shop/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 admin.site.site_header = "Game Shop Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.jwt")),
+    path("api/auth/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls.jwt")),
     path("api/", include("games.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("api-auth/", include("rest_framework.urls"))]
