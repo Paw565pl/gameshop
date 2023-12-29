e077d90224586112769f8bae118a71aab624c24e
Paw565pl <paw565pl@gmail.com>
2023-12-29 17:13:08 +0100

    created support ticket admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from core.mixins import ExportJsonMixin
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, SupportTicket, SupportTicketMessage
+from .serializers import OrderSerializer, SupportTicketSerializer
 
 
 # Register your models here.
@@ -21,3 +23,26 @@ class OrderAdmin(admin.ModelAdmin, ExportJsonMixin):
             },
         ),
     )
+
+
+@admin.register(SupportTicket)
+class SupportTicketAdmin(admin.ModelAdmin, ExportJsonMixin):
+    serializer_class = SupportTicketSerializer
+    list_display = ["id", "status", "created_at", "updated_at"]
+    list_filter = ["status", "created_at"]
+    readonly_fields = ["id", "created_at", "updated_at", "order_link"]
+    fieldsets = (
+        ("General Information", {"fields": ("id", "order_link", "status", "messages")}),
+        ("Timestamps", {"fields": ("created_at", "updated_at")}),
+    )
+
+    @staticmethod
+    def order_link(obj):
+        order_id = obj.order.get().id
+        url = reverse("admin:orders_order_change", args=[order_id])
+        return mark_safe('<a href="{}">{}</a>'.format(url, "Order"))
+
+    order_link.short_description = "Order"
+
+
+admin.site.register(SupportTicketMessage)
