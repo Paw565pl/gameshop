b99afb9bcf3de5674bd2657ff7d730962826b576
Paw565pl <paw565pl@gmail.com>
2023-12-30 18:44:05 +0100

    added drf spectacular

--- game_shop/urls.py
+++ game_shop/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 admin.site.site_header = "Game Shop Admin"
 
@@ -25,7 +30,16 @@ urlpatterns = [
     path("api/auth/", include("djoser.urls.jwt")),
     path("api/", include("games.urls")),
     path("api/", include("orders.urls")),
+    # drf spectacular views
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [path("api-auth/", include("rest_framework.urls"))]
