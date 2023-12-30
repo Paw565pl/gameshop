31f2fac20506e144f8c438c6da6f6dfc703f3adc
Paw565pl <paw565pl@gmail.com>
2023-12-30 21:00:37 +0100

    configured production settings

--- game_shop/settings/production.py
+++ game_shop/settings/production.py
@@ -1,27 +1,31 @@
 from .common import *
+from environ import Env
 
-SECRET_KEY = "django-insecure-k32ernz5o^k8xa$-p%wb$7=k-6&_36p-63-!gkmy==v%srrjq8"
+env = Env(MONGO_PORT=(int, 27017), DJANGO_LOG_LEVEL=(str, "WARNING"))
+Env.read_env(BASE_DIR / ".env")
 
-ALLOWED_HOSTS = []
+SECRET_KEY = env("SECRET_KEY")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "djongo",
-#         "NAME": "game_shop_db",
-#         "ENFORCE_SCHEMA": True,
-#         "CLIENT": {
-#             "host": env("MONGO_HOST"),
-#             "port": env("MONGO_PORT"),
-#             # 'username': 'db-username',
-#             # 'password': 'password',
-#             # 'authSource': 'db-name',
-#             # 'authMechanism': 'SCRAM-SHA-1'
-#         },
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "djongo",
+        "NAME": "game_shop_db",
+        "ENFORCE_SCHEMA": True,
+        "CLIENT": {
+            "host": env("MONGO_HOST"),
+            "port": env("MONGO_PORT"),
+            # 'username': 'db-username',
+            # 'password': 'password',
+            # 'authSource': 'db-name',
+            # 'authMechanism': 'SCRAM-SHA-1'
+        },
+    }
+}
 
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
@@ -31,4 +35,29 @@ CSRF_COOKIE_SECURE = True
 
 SESSION_COOKIE_SECURE = True
 
-# LOGGING
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": "general.log",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "file"],
+            "level": env("DJANGO_LOG_LEVEL"),
+        },
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{asctime} ({levelname}) - {name} - {message}",
+            "style": "{",
+        }
+    },
+}
