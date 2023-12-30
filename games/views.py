fff35c3a0ba66fb185f5053ecf0986d6efba69ea
Paw565pl <paw565pl@gmail.com>
2023-12-30 13:12:48 +0100

    created read only view sets for genres, platforms and developers

--- games/views.py
+++ games/views.py
@@ -1,18 +1,33 @@
 from rest_framework import viewsets, permissions
 
 from .filters import GameFilter
-from .models import Game, Review, Screenshot
+from .models import Game, Review, Screenshot, Genre, Platform, Developer
 from .permissions import IsAuthorOrReadOnly
-from .serializers import GameSerializer, ReviewSerializer, ScreenshotSerializer
+from .serializers import (
+    GameSerializer,
+    ReviewSerializer,
+    ScreenshotSerializer,
+    GenreSerializer,
+    PlatformSerializer,
+    DeveloperSerializer,
+)
 from .utils import check_if_game_exists
 
 
 # Create your views here.
-class GameViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Game.objects.order_by("id")
-    serializer_class = GameSerializer
-    filterset_class = GameFilter
-    ordering_fields = ["name", "released", "metacritic", "price"]
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Genre.objects.all().order_by("id")
+    serializer_class = GenreSerializer
+
+
+class PlatformViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Platform.objects.all().order_by("id")
+    serializer_class = PlatformSerializer
+
+
+class DeveloperViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Developer.objects.all().order_by("id")
+    serializer_class = DeveloperSerializer
 
 
 class ScreenshotViewSet(viewsets.ReadOnlyModelViewSet):
@@ -40,3 +55,10 @@ class ReviewViewSet(viewsets.ModelViewSet):
         check_if_game_exists(game_id)
 
         return super().create(request, *args, **kwargs)
+
+
+class GameViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Game.objects.order_by("id")
+    serializer_class = GameSerializer
+    filterset_class = GameFilter
+    ordering_fields = ["name", "released", "metacritic", "price"]
