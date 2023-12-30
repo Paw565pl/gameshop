75cc0bc054561fcf9a3350646a28c864b1d74d39
Paw565pl <paw565pl@gmail.com>
2023-12-30 18:31:59 +0100

    created separate settings for production and development

---
+++ game_shop/settings/development.py
@@ -0,0 +1,22 @@
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-k32ernz5o^k8xa$-p%wb$7=k-6&_36p-63-!gkmy==v%srrjq8"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "djongo",
+        "NAME": "game_shop_db",
+        "ENFORCE_SCHEMA": True,
+        "CLIENT": {
+            "host": "localhost",
+            "port": 27017,
+        },
+    }
+}
---
+++ game_shop/settings/production.py
@@ -0,0 +1,30 @@
+from .common import *
+
+SECRET_KEY = "django-insecure-k32ernz5o^k8xa$-p%wb$7=k-6&_36p-63-!gkmy==v%srrjq8"
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "djongo",
+#         "NAME": "game_shop_db",
+#         "ENFORCE_SCHEMA": True,
+#         "CLIENT": {
+#             "host": env("MONGO_HOST"),
+#             "port": env("MONGO_PORT"),
+#             # 'username': 'db-username',
+#             # 'password': 'password',
+#             # 'authSource': 'db-name',
+#             # 'authMechanism': 'SCRAM-SHA-1'
+#         },
+#     }
+# }
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
+# LOGGING
