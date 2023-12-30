0c64d204069634d56c9d2e4a1763583fbef54c2d
Paw565pl <paw565pl@gmail.com>
2023-12-30 18:44:05 +0100

    added drf spectacular

--- game_shop/settings/common.py
+++ game_shop/settings/common.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django_filters",
     "djoser",
     "rest_framework_simplejwt",
+    "drf_spectacular",
     "core",
     "games",
     "orders",
@@ -116,4 +117,5 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
