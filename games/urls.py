b163d956a618ef5dc40469eaab5c6e76771669bf
Paw565pl <paw565pl@gmail.com>
2023-12-30 13:12:48 +0100

    created read only view sets for genres, platforms and developers

--- games/urls.py
+++ games/urls.py
@@ -4,12 +4,15 @@ from . import views
 
 router = routers.DefaultRouter()
 
+router.register(r"genres", views.GenreViewSet)
+router.register(r"platforms", views.PlatformViewSet)
+router.register(r"developers", views.DeveloperViewSet)
 router.register(r"games", views.GameViewSet)
 
 game_router = routers.NestedDefaultRouter(router, r"games", lookup="game")
-game_router.register(r"reviews", views.ReviewViewSet, basename="game-reviews")
 game_router.register(
     r"screenshots", views.ScreenshotViewSet, basename="game-screenshots"
 )
+game_router.register(r"reviews", views.ReviewViewSet, basename="game-reviews")
 
 urlpatterns = router.urls + game_router.urls
