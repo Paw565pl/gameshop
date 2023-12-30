1aaddf1b8309660aedb992b6d6899e4b669ecc95
Paw565pl <paw565pl@gmail.com>
2023-12-30 15:53:36 +0100

    created favourite game view set

--- games/serializers.py
+++ games/serializers.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django.contrib.auth import get_user
 from rest_framework import serializers
 
 from .models import Game, Genre, Platform, Developer, Screenshot, Review
@@ -88,3 +89,27 @@ class SimpleGameSerializer(serializers.ModelSerializer):
             "background_image",
             "price",
         ]
+
+
+class AddFavouriteGameSerializer(GameSerializer):
+    game_id = serializers.IntegerField(write_only=True)
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        game_id = validated_data["game_id"]
+
+        try:
+            game = Game.objects.get(pk=game_id)
+        except Game.DoesNotExist:
+            raise serializers.ValidationError("Game not found")
+
+        has_game = user.favourite_games.filter(pk=game_id).count() != 0
+        if has_game:
+            raise serializers.ValidationError("Game already added to favourites")
+
+        user.favourite_games.add(game)
+        return game
+
+    class Meta:
+        model = Game
+        fields = ["game_id"]
