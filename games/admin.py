b47dbc817d321122635a2e4c9acad723743ad580
Paw565pl <paw565pl@gmail.com>
2023-12-30 12:47:33 +0100

    refactored game admin

--- games/admin.py
+++ games/admin.py
@@ -2,12 +2,14 @@ from django import forms
 from django.contrib import admin
 
 from core.mixins import ExportJsonMixin
-from .models import Game, Genre, Platform, Developer
+from .models import Game, Genre, Platform, Developer, Review, Screenshot
 from .serializers import (
     GameSerializer,
     GenreSerializer,
     PlatformSerializer,
     DeveloperSerializer,
+    ScreenshotSerializer,
+    ReviewSerializer,
 )
 
 
@@ -17,6 +19,35 @@ class CommonAdmin(admin.ModelAdmin, ExportJsonMixin):
     list_display = ["name"]
     ordering = ["id"]
     search_fields = ["name__icontains", "slug__icontains"]
+
+
+@admin.register(Genre)
+class GenreAdmin(CommonAdmin):
+    serializer_class = GenreSerializer
+
+
+@admin.register(Platform)
+class PlatformAdmin(CommonAdmin):
+    serializer_class = PlatformSerializer
+
+
+@admin.register(Developer)
+class DeveloperAdmin(CommonAdmin):
+    serializer_class = DeveloperSerializer
+
+
+@admin.register(Screenshot)
+class ScreenshotAdmin(admin.ModelAdmin, ExportJsonMixin):
+    serializer_class = ScreenshotSerializer
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin, ExportJsonMixin):
+    serializer_class = ReviewSerializer
+    list_display = ["author"]
+    list_filter = ["is_positive"]
+    search_fields = ["author__icontains"]
+    ordering = ["id"]
 
 
 class GameAdminForm(forms.ModelForm):
@@ -34,19 +65,5 @@ class GameAdminForm(forms.ModelForm):
 class GameAdmin(CommonAdmin):
     serializer_class = GameSerializer
     form = GameAdminForm
-    list_filter = ["genres", "platforms", "developers"]
-
-
-@admin.register(Genre)
-class GenreAdmin(CommonAdmin):
-    serializer_class = GenreSerializer
-
-
-@admin.register(Platform)
-class PlatformAdmin(CommonAdmin):
-    serializer_class = PlatformSerializer
-
-
-@admin.register(Developer)
-class DeveloperAdmin(CommonAdmin):
-    serializer_class = DeveloperSerializer
+    list_filter = ["released", "genres", "platforms", "developers"]
+    autocomplete_fields = ["reviews"]
