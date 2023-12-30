a0b6528807761289a8cd051f2a4c29c5bcb2c7c1
Paw565pl <paw565pl@gmail.com>
2023-12-30 18:31:59 +0100

    created separate settings for production and development

--- game_shop/wsgi.py
+++ game_shop/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "game_shop.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "game_shop.settings.development")
 
 application = get_wsgi_application()
