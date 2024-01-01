3d2d9733ccc497c63de8b7e8cb4aed26b4eeaa11
Paw565pl <paw565pl@gmail.com>
2024-01-01 15:52:01 +0100

    removed dependency on game id

--- games/tests/test_add_favourite_game.py
+++ games/tests/test_add_favourite_game.py
@@ -4,46 +4,48 @@ from games.models import Game
 
 class TestAddFavouriteGame(AuthenticatedAPITestCase):
     url = "/api/favourite-games/"
-    test_data = {"game_id": 1}
+
+    def get_test_data(self):
+        test_data = {"game_id": self.game.id}
+        return test_data
 
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.game = Game.objects.create(
-            id=1, name="test", name_original="test", slug="test", price=1
+            name="test", name_original="test", slug="test", price=1
         )
 
     def test_no_user_returns_401(self):
-        response = self.client.post(self.url, self.test_data)
+        response = self.client.post(self.url, self.get_test_data())
         self.assertEqual(
             response.status_code, 401, f"Invalid status code: {response.status_code}"
         )
 
     def test_no_game_returns_400(self):
         self.authenticate()
-        response = self.client.post(self.url, {"game_id": 2})
+        response = self.client.post(self.url, {"game_id": self.game.id + 1})
         self.assertEqual(
             response.status_code, 400, f"Invalid status code: {response.status_code}"
         )
 
     def test_add_twice_returns_400(self):
         self.authenticate()
-        self.client.post(self.url, self.test_data)
-        response = self.client.post(self.url, self.test_data)
+        self.client.post(self.url, self.get_test_data())
+        response = self.client.post(self.url, self.get_test_data())
         self.assertEqual(
             response.status_code, 400, f"Invalid status code: {response.status_code}"
         )
 
     def test_add_favourite_game_returns_201(self):
         self.authenticate()
-        response = self.client.post(self.url, self.test_data)
-        response_json = response.json()
+        response = self.client.post(self.url, self.get_test_data())
         favourite_games = self.client.get(self.url).json()
 
         self.assertEqual(
             response.status_code, 201, f"Invalid status code: {response.status_code}"
         )
-        self.assertEqual(response_json["id"], 1, "Invalid response!")
-        self.assertEqual(response_json["name"], self.game.name, "Invalid response!")
         self.assertEqual(favourite_games["count"], 1, "Game was not added!")
-        self.assertEqual(favourite_games["results"][0]["id"], 1, "Game was not added!")
+        self.assertEqual(
+            favourite_games["results"][0]["id"], self.game.id, "Game was not added!"
+        )
--- games/tests/test_remove_favourite_game.py
+++ games/tests/test_remove_favourite_game.py
@@ -3,31 +3,35 @@ from games.models import Game
 
 
 class TestRemoveFavouriteGame(AuthenticatedAPITestCase):
-    url = "/api/favourite-games/1/"
+    def get_url(self):
+        url = f"/api/favourite-games/{self.game.id}/"
+        return url
 
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.game = Game.objects.create(
-            id=1, name="test", name_original="test", slug="test", price=1
+            name="test", name_original="test", slug="test", price=1
         )
 
     def setUp(self):
         self.authenticate()
-        self.client.post("/api/favourite-games/", {"game_id": 1})
+        self.client.post("/api/favourite-games/", {"game_id": self.game.id})
 
     def test_no_favourite_game_returns_404(self):
-        response = self.client.delete("/api/favourite-games/2/")
+        response = self.client.delete(f"/api/favourite-games/{self.game.id + 1}/")
         self.assertEqual(
             response.status_code, 404, f"Invalid status code: {response.status_code}"
         )
 
     def test_delete_favourite_game_returns_204(self):
-        response = self.client.delete(self.url)
+        response = self.client.delete(self.get_url())
         games = self.client.get("/api/games/").json()
 
         self.assertEqual(
             response.status_code, 204, f"Invalid status code: {response.status_code}"
         )
         self.assertEqual(games["count"], 1, "Game object was deleted!")
-        self.assertEqual(games["results"][0]["id"], 1, "Game object was deleted!")
+        self.assertEqual(
+            games["results"][0]["id"], self.game.id, "Game object was deleted!"
+        )
