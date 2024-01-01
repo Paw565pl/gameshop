cd754fdf7e5db3689e679feb3f6f3bf1e92c873c
Paw565pl <paw565pl@gmail.com>
2024-01-01 14:09:14 +0100

    refactored test names

--- games/tests/test_create_review.py
+++ games/tests/test_create_review.py
@@ -14,13 +14,13 @@ class TestCreateReview(AuthenticatedAPITestCase):
             id=1, name="test", name_original="test", slug="test", price=1
         )
 
-    def test_create_review_no_user_returns_401(self):
+    def test_no_user_returns_401(self):
         response = self.client.post(self.url, self.test_review)
         self.assertEqual(
             response.status_code, 401, f"Invalid status code: {response.status_code}"
         )
 
-    def test_create_review_twice_returns_400(self):
+    def test_create_twice_returns_400(self):
         self.authenticate()
         self.client.post(self.url, self.test_review)
         response = self.client.post(self.url, self.test_review)
@@ -29,7 +29,7 @@ class TestCreateReview(AuthenticatedAPITestCase):
             response.status_code, 400, f"Invalid status code: {response.status_code}"
         )
 
-    def test_create_review_no_game_returns_404(self):
+    def test_no_game_returns_404(self):
         self.authenticate()
         response = self.client.post("/api/games/2/reviews/", self.test_review)
 
@@ -37,7 +37,7 @@ class TestCreateReview(AuthenticatedAPITestCase):
             response.status_code, 404, f"Invalid status code: {response.status_code}"
         )
 
-    def test_create_empty_review_returns_400(self):
+    def test_empty_review_returns_400(self):
         self.authenticate()
 
         response = self.client.post(self.url, {})
