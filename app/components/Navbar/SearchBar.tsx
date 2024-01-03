2eef980fdd5e6fb969bdd1007051a082923abfb7
Paw565pl <paw565pl@gmail.com>
2024-01-03 18:04:31 +0100

    feat: create search bar

---
+++ app/components/Navbar/SearchBar.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { GoSearch } from "react-icons/go";
+
+const SearchBar = () => {
+  return (
+    <search className="flex items-center w-full mx-6 relative">
+      <GoSearch
+        className="absolute left-2 text-3xl cursor-pointer"
+        onClick={() => console.log("search")}
+      />
+      <input
+        type="text"
+        placeholder="Search for games here"
+        className="input input-bordered input-base-300 rounded-xl w-full focus:outline-none focus:border-primary pl-11"
+      />
+    </search>
+  );
+};
+
+export default SearchBar;
