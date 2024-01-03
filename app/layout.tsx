96bc4534525a30415c77c61361d79e1038247f1c
Paw565pl <paw565pl@gmail.com>
2024-01-03 18:05:03 +0100

    feat(layout): add navbar to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+import Navbar from "./components/Navbar/Navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,14 +10,21 @@ export const metadata: Metadata = {
   title: "GameShop",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const RootLayout = ({ children }: LayoutProps) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-6xl xl:px-0`}
+      >
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
