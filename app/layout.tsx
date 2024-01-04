f3744dac5c012b5d946249bd5817115bdc0cb865
Paw565pl <paw565pl@gmail.com>
2024-01-04 16:56:02 +0100

    refactor: lowercase dir name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
