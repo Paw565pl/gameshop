998969100fa2b6ac6eb247da264503a1aeb44e5c
Paw565pl <paw565pl@gmail.com>
2024-01-05 17:00:43 +0100

    added django cors headers for development

--- game_shop/settings/development.py
+++ game_shop/settings/development.py
@@ -8,6 +8,11 @@ SECRET_KEY = "django-insecure-k32ernz5o^k8xa$-p%wb$7=k-6&_36p-63-!gkmy==v%srrjq8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
