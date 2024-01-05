d0db18e46d6f41a9fee857c521c43e1e05600954
Paw565pl <paw565pl@gmail.com>
2024-01-05 22:49:25 +0100

    refactor: use of game object in game card

--- app/components/gameCard/GameCard.tsx
+++ app/components/gameCard/GameCard.tsx
@@ -1,36 +1,56 @@
-import spiderman from "@/public/spiderman.jpg";
+import Game from "@/app/entities/Game";
+import noImagePlaceholder from "@/public/no_image_placeholder.png";
 import Image from "next/image";
+import Link from "next/link";
 import { CgDetailsMore } from "react-icons/cg";
 import MetacriticBadge from "../common/MetacriticBadge";
 
-const GameCard = () => {
+interface GameCardProps {
+  game: Game;
+}
+
+const GameCard = ({ game }: GameCardProps) => {
   return (
     <div className="card bg-base-100 shadow-xl md:card-side">
-      <figure>
-        <Image src={spiderman} alt="game image" />
+      <figure className="md:w-3/4">
+        <Link href={`/game/${game.slug}`} className="h-min w-full">
+          <Image
+            src={game.background_image || noImagePlaceholder}
+            alt={`${game.name} image`}
+            width={400}
+            height={400}
+            className="w-full"
+          />
+        </Link>
       </figure>
-      <div className="card-body gap-0">
+      <div className="card-body w-full gap-0">
         <h2 className="card-title text-4xl sm:text-5xl">
-          Marvel&apos;s Spider-Man
+          <Link href={`/game/${game.slug}`}>{game.name}</Link>
         </h2>
-        <div className="text-xs text-base-300">(Marvel&apos;s Spider-Man)</div>
-        <dl className="my-4 grid w-full grid-cols-2 gap-0 text-sm sm:w-3/4 sm:text-base md:w-full xl:w-1/2">
+        <div className="text-xs text-base-300">{game.name_original}</div>
+        <dl className="my-4 grid w-full grid-cols-2 gap-0 text-sm sm:w-3/4 sm:text-base md:w-full xl:w-3/4">
           <dt>Release date:</dt>
-          <dd>2018-09-07</dd>
+          <dd>{game.released ? game.released : "N/A"}</dd>
           <dt>Main developer:</dt>
-          <dd>Insomniac Games</dd>
+          <dd>{game.developers[0].name}</dd>
           <dt>Main genre:</dt>
-          <dd>Action</dd>
+          <dd>{game.genres[0].name}</dd>
         </dl>
         <div className="card-actions mb-1 h-full items-end justify-end sm:text-xl">
-          20.99 PLN
+          {game.price} PLN
         </div>
         <div className="card-actions justify-between">
-          <MetacriticBadge metacritic={87} />
-          <button className="btn btn-primary btn-sm h-full rounded sm:btn-md">
-            <CgDetailsMore className="text-xl" />
-            Details
-          </button>
+          {game.metacritic ? (
+            <MetacriticBadge metacritic={game.metacritic} />
+          ) : (
+            <div></div>
+          )}
+          <Link href={`/game/${game.slug}`}>
+            <button className="btn btn-primary btn-sm h-full rounded sm:btn-md">
+              <CgDetailsMore className="text-xl" />
+              Details
+            </button>
+          </Link>
         </div>
       </div>
     </div>
