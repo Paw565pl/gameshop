9f028d796cd2781e6d8172288c67ac366cf6e60a
Paw565pl <paw565pl@gmail.com>
2024-01-06 17:32:21 +0100

    feat: add handle submit to form

--- app/register/components/RegisterForm.tsx
+++ app/register/components/RegisterForm.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import FormInput from "@/app/components/common/FormInput";
+import Toast, { ToastProps } from "@/app/components/common/Toast";
+import useRegisterUser from "@/app/hooks/client/useRegisterUser";
 import registerUserSchema, {
   RegisterUserValues,
 } from "@/app/schemas/registerUserSchema";
 import { Form, Formik } from "formik";
 import Link from "next/link";
+import { useState } from "react";
 
 const initialValues: RegisterUserValues = {
   username: "",
@@ -14,13 +17,50 @@ const initialValues: RegisterUserValues = {
 };
 
 const RegisterForm = () => {
+  const [toast, setToast] = useState<ToastProps | null>(null);
+  const { mutate: registerUser } = useRegisterUser();
+
+  const handleSubmit = (formValues: RegisterUserValues) => {
+    registerUser(formValues, {
+      onSuccess: () => {
+        setToast({
+          variant: "success",
+          children:
+            "Your account has been succesfully created! You can now log in.",
+        });
+      },
+      onError: (error) => {
+        if (error.response?.status === 500) {
+          setToast({
+            variant: "error",
+            children:
+              "Something went wrong. There is already a user with that username or email.",
+          });
+        } else {
+          const errors = error.response?.data;
+          const errorsString = Object.entries(errors!)
+            .map(([_, values]) => `${values.join("\n")}`)
+            .join("\n");
+
+          setToast({
+            variant: "error",
+            children: errorsString,
+          });
+        }
+      },
+    });
+
+    setTimeout(() => setToast(null), 5000);
+  };
+
   return (
     <>
+      {toast && <Toast variant={toast.variant}>{toast.children}</Toast>}
       <h1 className="mb-4 text-center text-6xl font-bold">Register Form</h1>
       <Formik
         initialValues={initialValues}
         validationSchema={registerUserSchema}
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
       >
         <Form className="mx-auto sm:w-2/3">
           <FormInput labelText="Username" name="username" type="text" />
