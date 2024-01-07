daf14c3f637fb89e18c29fe9a72e2d6300cc4c41
Paw565pl <paw565pl@gmail.com>
2024-01-07 20:36:07 +0100

    create private route component

---
+++ app/components/common/PrivateRoute.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { AuthContext } from "@/app/contexts/AuthContextProvider";
+import { usePathname, useRouter } from "next/navigation";
+import { ReactNode, useContext, useLayoutEffect, useState } from "react";
+
+interface PrivateRouteProps {
+  children: ReactNode;
+}
+
+const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const [loading, setLoading] = useState(true);
+  const { isAuthenticated } = useContext(AuthContext);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useLayoutEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login?returnUrl=" + pathname);
+    } else {
+      setLoading(false);
+    }
+  }, [isAuthenticated, pathname, router]);
+
+  if (loading) {
+    return null;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
