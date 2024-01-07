6234b56c241bc7aa5e9766da1065223012f33ba1
Paw565pl <paw565pl@gmail.com>
2024-01-07 20:34:50 +0100

    feat: add handle submit to login form

--- app/login/components/LoginForm.tsx
+++ app/login/components/LoginForm.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import FormInput from "@/app/components/common/FormInput";
+import Toast, { ToastProps } from "@/app/components/common/Toast";
+import useLoginUser from "@/app/hooks/client/useLoginUser";
 import loginUserSchema, {
   LoginUserValues,
 } from "@/app/schemas/loginUserSchema";
 import { Form, Formik } from "formik";
 import Link from "next/link";
+import { useState } from "react";
 
 const initialValues: LoginUserValues = {
   username: "",
@@ -13,13 +16,37 @@ const initialValues: LoginUserValues = {
 };
 
 const LoginForm = () => {
+  const [toast, setToast] = useState<ToastProps | null>(null);
+  const { mutate: loginUser } = useLoginUser();
+
+  const handleSubmit = (formValues: LoginUserValues) => {
+    loginUser(formValues, {
+      onError: (error) => {
+        if (error.response?.status === 401) {
+          setToast({
+            variant: "error",
+            children: "Invalid username or password.",
+          });
+        } else {
+          setToast({
+            variant: "error",
+            children: "Something went wrong. Please try again later.",
+          });
+        }
+      },
+    });
+
+    setTimeout(() => setToast(null), 5000);
+  };
+
   return (
     <>
+      {toast && <Toast variant={toast.variant}>{toast.children}</Toast>}
       <h1 className="mb-4 text-center text-6xl font-bold">Login Form</h1>
       <Formik
         initialValues={initialValues}
         validationSchema={loginUserSchema}
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
       >
         <Form className="mx-auto sm:w-2/3">
           <FormInput labelText="Username" name="username" type="text" />
