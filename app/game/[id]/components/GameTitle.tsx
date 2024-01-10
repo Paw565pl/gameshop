6554ae260533275087ea3e9e68007bf387dd5f8b
Paw565pl <paw565pl@gmail.com>
2024-01-10 12:38:39 +0100

    style: changed game title to h1

--- app/game/[id]/components/GameTitle.tsx
+++ app/game/[id]/components/GameTitle.tsx
@@ -10,7 +10,7 @@ const GameTitle = ({ name, nameOriginal, metacritic }: GameTitleProps) => {
   return (
     <header>
       <div className="flex items-center gap-2">
-        <h2 className="text-4xl font-semibold sm:text-5xl">{name}</h2>
+        <h1 className="text-4xl font-semibold sm:text-5xl">{name}</h1>
         <MetacriticBadge metacritic={metacritic} />
       </div>
       <div className="pl-1 text-xs text-base-300">{nameOriginal}</div>
