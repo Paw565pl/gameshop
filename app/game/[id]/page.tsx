144ab4a38a28a17d4d2b94e3b2aa41a5e6d3e058
Paw565pl <paw565pl@gmail.com>
2024-01-10 17:14:13 +0100

    refactor: move components to sub dirs

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -4,9 +4,9 @@ import {
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
-import GameDetail from "./components";
+import GameDetail from "./components/GameDetail";
 import ReviewsList from "./components/ReviewsList";
-import AddReviewForm from "./components/ReviewsList/AddReviewForm";
+import AddReviewForm from "./components/AddReviewForm";
 
 interface GameDetailPageProps {
   params: { id: number };
