494c77a963e5db3fcb62318e3b34c194888058be
Paw565pl <paw565pl@gmail.com>
2024-01-12 10:57:56 +0100

    feat: add handle submit to change user info

--- app/profile/components/Personal.tsx
+++ app/profile/components/Personal.tsx
@@ -1,11 +1,20 @@
 import FormInput from "@/app/components/common/FormInput";
 import FormSubmitButton from "@/app/components/common/FormSubmitButton";
+import Toast, { ToastProps } from "@/app/components/common/Toast";
 import useFetchUserInfo from "@/app/hooks/client/useFetchUserInfo";
-import registerUserSchema from "@/app/schemas/registerUserSchema";
+import useUpdateUserInfo from "@/app/hooks/client/useUpdateUserInfo";
+import registerUserSchema, {
+  RegisterUserValues,
+} from "@/app/schemas/registerUserSchema";
 import { Form, Formik } from "formik";
+import { useState } from "react";
 
 const Personal = () => {
-  const { data: userInfo } = useFetchUserInfo();
+  const [toast, setToast] = useState<ToastProps | null>(null);
+  const { data: userInfo, isLoading } = useFetchUserInfo();
+  const { mutate: updateUserInfo } = useUpdateUserInfo();
+
+  if (isLoading) return <p>Loading...</p>;
 
   const initialValues = {
     username: userInfo?.username || "",
@@ -13,23 +22,40 @@ const Personal = () => {
     password: "",
   };
 
+  const handleSubmit = (formValues: RegisterUserValues) => {
+    updateUserInfo(formValues, {
+      onSuccess: () =>
+        setToast({ variant: "success", children: "Changes saved." }),
+      onError: () =>
+        setToast({
+          variant: "error",
+          children: "Something went wrong. Please provide valid data.",
+        }),
+    });
+
+    setTimeout(() => setToast(null), 5000);
+  };
+
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={registerUserSchema}
-      onSubmit={() => {}}
-    >
-      <Form>
-        <FormInput labelText="Username" name="username" type="text" />
-        <FormInput labelText="E-mail" name="email" type="text" />
-        <FormInput
-          labelText="Current password"
-          name="password"
-          type="password"
-        />
-        <FormSubmitButton>Save changes</FormSubmitButton>
-      </Form>
-    </Formik>
+    <>
+      {toast && <Toast variant={toast.variant}>{toast.children}</Toast>}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={registerUserSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <FormInput labelText="Username" name="username" type="text" />
+          <FormInput labelText="E-mail" name="email" type="text" />
+          <FormInput
+            labelText="Current password"
+            name="password"
+            type="password"
+          />
+          <FormSubmitButton>Save changes</FormSubmitButton>
+        </Form>
+      </Formik>
+    </>
   );
 };
 
