6c0824eba2daf75119e448b9be68752abf71429f
Paw565pl <paw565pl@gmail.com>
2024-01-12 11:16:19 +0100

    feat: create schema for changing user password

---
+++ app/schemas/changeUserPasswordSchema.ts
@@ -0,0 +1,16 @@
+import { InferType, object, string } from "yup";
+
+const changeUserPasswordSchema = object({
+  currentPassword: string()
+    .required("password is required")
+    .min(8, "password's minimum length is 8 characters"),
+  newPassword: string()
+    .required("password is required")
+    .min(8, "password's minimum length is 8 characters"),
+});
+
+export type ChangeUserPasswordValues = InferType<
+  typeof changeUserPasswordSchema
+>;
+
+export default changeUserPasswordSchema;
