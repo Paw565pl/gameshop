5be7110f4937f43c84d08ce9d999d89ca086f6ac
Paw565pl <paw565pl@gmail.com>
2024-01-14 16:18:32 +0100

    feat: build search bar behaviour with debouncing

--- app/components/navbar/SearchBar.tsx
+++ app/components/navbar/SearchBar.tsx
@@ -1,18 +1,32 @@
 "use client";
 
+import { actions } from "@/app/redux/gameQuerySlice";
+import { useAppDispatch } from "@/app/redux/hooks";
+import debounce from "just-debounce-it";
+import { ChangeEvent, useRef } from "react";
 import { GoSearch } from "react-icons/go";
 
 const SearchBar = () => {
+  const dispatch = useAppDispatch();
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  const handleChange = debounce((e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    dispatch(actions.setName(value));
+  }, 300);
+
   return (
     <search className="relative mx-6 flex w-full items-center">
       <GoSearch
         className="absolute left-2 cursor-pointer text-3xl"
-        onClick={() => console.log("search")}
+        onClick={() => searchInput.current?.focus()}
       />
       <input
         type="text"
         placeholder="Search for games here"
         className="input-base-300 input input-bordered w-full rounded-xl pl-11 focus:border-accent focus:outline-none"
+        ref={searchInput}
+        onChange={handleChange}
       />
     </search>
   );
