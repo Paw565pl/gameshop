ac66adf3dbf0d085dcb93b26f06f8bd215be8206
Paw565pl <paw565pl@gmail.com>
2024-01-14 14:24:08 +0100

    feat: add prefetching genres to root page

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,15 @@ import {
 import SideFilters from "./components/SideFilters";
 import GameCardGrid from "./components/gameCardGrid";
 import usePrefetchGames from "./hooks/server/usePrefetchGames";
+import usePrefetchGenres from "./hooks/server/usePrefetchGenres";
 
 const RootPage = async () => {
   const queryClient = new QueryClient();
-  await usePrefetchGames(queryClient);
+
+  const games = usePrefetchGames(queryClient);
+  const genres = usePrefetchGenres(queryClient);
+
+  await Promise.all([games, genres]);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
