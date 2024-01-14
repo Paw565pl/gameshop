2f1e23947127aead533d641331ebbc596e1139a9
Paw565pl <paw565pl@gmail.com>
2024-01-14 14:34:52 +0100

    feat: add prefetching platforms to root page

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,16 @@ import SideFilters from "./components/SideFilters";
 import GameCardGrid from "./components/gameCardGrid";
 import usePrefetchGames from "./hooks/server/usePrefetchGames";
 import usePrefetchGenres from "./hooks/server/usePrefetchGenres";
+import usePrefetchPlatforms from "./hooks/server/usePrefetchPlatforms";
 
 const RootPage = async () => {
   const queryClient = new QueryClient();
 
   const games = usePrefetchGames(queryClient);
   const genres = usePrefetchGenres(queryClient);
+  const platforms = usePrefetchPlatforms(queryClient);
 
-  await Promise.all([games, genres]);
+  await Promise.all([games, genres, platforms]);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
