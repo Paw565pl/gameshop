d18c075cbfc67bb1f2aba1628e48073c0f54643e
Paw565pl <paw565pl@gmail.com>
2024-01-14 18:27:12 +0100

    feat: add proper logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,5 +1,13 @@
+import logo from "@/public/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+
 const Logo = () => {
-  return <h1>Logo</h1>;
+  return (
+    <Link href="/" className="w-3/4 sm:w-1/2 md:w-1/3">
+      <Image src={logo} alt="logo"></Image>
+    </Link>
+  );
 };
 
 export default Logo;
