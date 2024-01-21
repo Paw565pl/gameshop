1ddd3758650bf2e373c4652e01c6aab85332b9a0
Paw565pl <paw565pl@gmail.com>
2024-01-21 17:22:30 +0100

    fix: adjusted loading state

--- app/profile/components/Address.tsx
+++ app/profile/components/Address.tsx
@@ -9,10 +9,10 @@ import { useState } from "react";
 
 const Address = () => {
   const [toast, setToast] = useState<ToastProps | null>(null);
-  const { data: userAddress, isLoading } = useFetchUserAddress();
+  const { data: userAddress, isFetching } = useFetchUserAddress();
   const { mutate: createOrUpdateUserAddress } = useCreateOrUpdateUserAddress();
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isFetching) return <p>Loading...</p>;
 
   const initialValues: AddressValues = {
     first_name: userAddress?.first_name || "",
