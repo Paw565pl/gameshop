6743149bef41d298d73a5ab07dbe54b8750d528e
Paw565pl <paw565pl@gmail.com>
2024-01-21 19:42:16 +0100

    refactor: split cart item entity

--- app/entities/CartItem.ts
+++ app/entities/CartItem.ts
@@ -1,18 +1,18 @@
+import Platform from "./Platform";
+
+interface SimpleGame {
+  id: number;
+  name: string;
+  slug: string;
+  released: string | null;
+  background_image: string | null;
+  price: string;
+}
+
 interface CartItem {
   id: number;
-  game: {
-    id: number;
-    name: string;
-    slug: string;
-    released: string | null;
-    background_image: string | null;
-    price: string;
-  };
-  platform: {
-    id: number;
-    name: string;
-    slug: string;
-  };
+  game: SimpleGame;
+  platform: Platform;
   quantity: number;
   total_price: string;
 }
