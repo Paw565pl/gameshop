dd56847e8360b00ebf0ef4a77cc415d15eda554c
Paw565pl <paw565pl@gmail.com>
2024-01-23 15:53:41 +0100

    feat: create redirect to admin panel

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_GAME_SHOP_API_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_GAME_SHOP_API_URL}/api`,
 });
 
 export default apiService;
--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: `${process.env.NEXT_PUBLIC_GAME_SHOP_API_URL}/admin`,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
