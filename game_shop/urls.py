03c55f255a83249af38eb9b0357b1c6f8944f8c7
Paw565pl <paw565pl@gmail.com>
2024-08-09 15:25:58 +0200

    chore: change docs url

--- game_shop/urls.py
+++ game_shop/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -31,14 +30,14 @@ urlpatterns = [
     path("api/", include("games.urls")),
     path("api/", include("orders.urls")),
     # drf spectacular views
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
